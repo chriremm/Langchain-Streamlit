8fe736d54318b0ec69e899579aaa926d8c5825a8
Christian Remmelberger <christian.remmelberger@ar-lx0111.agile-robots.de>
2024-03-18 10:52:54 +0100

    major changes to simplify the code

--- pages/Chatbot with streaming.py
+++ pages/Chatbot with streaming.py
@@ -6,6 +6,8 @@ from langchain.schema import ChatMessage
 from langchain_openai import ChatOpenAI
 import streamlit as st
 
+st.header('Chatbot')
+
 class StreamHandler(BaseCallbackHandler):
     def __init__(self, container, initial_text=""):
         self.container = container
@@ -15,68 +17,32 @@ class StreamHandler(BaseCallbackHandler):
         self.text += token
         self.container.markdown(self.text)
 
-identificator = __file__
-if "current_page" not in st.session_state:
-    try:
-        del st.session_state["messages"]
-    except:
-        pass
-    st.session_state["current_page"] = identificator
-if st.session_state["current_page"] != identificator:
-    try:
-        st.cache_resource.clear()
-        del st.session_state["current_page"]
-        del st.session_state["messages"]
-    except:
-        pass
-st.header('Chatbot')
-models = ["gpt-3.5-turbo", "gpt-4"]
+class Chatbot_streaming:
+    def __init__(self):
+        utils.configure_openai_api_key()
+        utils.configure_model()
+        utils.configure_new_chat()
 
-def reset_chat():
-    if "messages" in st.session_state:
-        del st.session_state["messages"] 
+    @utils.enable_chat_history
+    def main(self):
 
-with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key:", type="password", placeholder="sk-...", value=st.session_state['OPENAI_API_KEY'] if 'OPENAI_API_KEY' in st.session_state else '',)
-    if openai_api_key.startswith('sk-'):
-        openai_model = st.selectbox(
-                "Select Model:",
-                models,
-                index = 0,
+        openai_api_key = st.session_state['OPENAI_API_KEY']
+        openai_model = st.session_state['OPENAI_MODEL']
+        utils.check_key()
+        utils.check_model()
+
+        user_query = st.chat_input(placeholder="Ask me anything!")
+        if user_query:
+            utils.display_msg(user_query, 'user')
+
+            with st.chat_message("assistant"):
+                stream_handler = StreamHandler(st.empty())
+                llm = ChatOpenAI(model_name=openai_model, openai_api_key=openai_api_key, streaming=True, callbacks=[stream_handler])
+                response = llm.invoke(st.session_state.messages)
+                st.session_state.messages.append(ChatMessage(role="assistant", content=response.content))
                 
-            )
-        st.markdown("<br><br>", unsafe_allow_html=True)
-        st.button("New Chat", on_click=reset_chat)
-    
-if openai_api_key.startswith('sk-'):
-    st.session_state['OPENAI_API_KEY'] = openai_api_key
-    if "cur_key" not in st.session_state or openai_api_key != st.session_state["cur_key"]:
-        st.session_state["cur_key"] = openai_api_key
-        reset_chat()
+        
 
-    if "cur_model" not in st.session_state or openai_model != st.session_state["cur_model"]:
-        st.session_state["cur_model"] = openai_model
-        reset_chat()
-
-    if "messages" not in st.session_state:
-        st.session_state["messages"] = [ChatMessage(role="assistant", content="How can I help you?")]
-
-    for msg in st.session_state.messages:
-        st.chat_message(msg.role).write(msg.content)
-
-    if prompt := st.chat_input():
-        st.session_state.messages.append(ChatMessage(role="user", content=prompt))
-        st.chat_message("user").write(prompt)
-
-        if not openai_api_key:
-            st.info("Please add your OpenAI API key to continue.")
-            st.stop()
-
-        with st.chat_message("assistant"):
-            stream_handler = StreamHandler(st.empty())
-            llm = ChatOpenAI(model_name=openai_model, openai_api_key=openai_api_key, streaming=True, callbacks=[stream_handler])
-            response = llm.invoke(st.session_state.messages)
-            st.session_state.messages.append(ChatMessage(role="assistant", content=response.content))
-else:
-    st.warning('Please enter a valid OpenAI API key!', icon='⚠️')
-    st.info("Obtain your key from this link: https://platform.openai.com/account/api-keys")
+if __name__ == "__main__":
+    bot = Chatbot_streaming()
+    bot.main()
--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 import os
 import random
 import streamlit as st
+from langchain.schema import ChatMessage
+
+models = ["gpt-3.5-turbo", "gpt-4"]
 
 def enable_chat_history(func):
     if os.environ.get("OPENAI_API_KEY"):
@@ -21,18 +24,14 @@ def enable_chat_history(func):
                 del st.session_state["messages"]
             except:
                 pass
-            # try:
-            #     del st.session_state["OPENAI_API_KEY"]
-            # except:
-            #     pass
 
 
-        # to show chat history on ui
+        # to show chat history on ui ChatMessage(role="assistant", content="How can I help you?")
         if "messages" not in st.session_state:
-            st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
-            st.rerun
-        for msg in st.session_state["messages"]:
-            st.chat_message(msg["role"]).write(msg["content"])
+            st.session_state["messages"] = [ChatMessage(role="assistant", content="How can I help you?")]
+            
+        for msg in st.session_state.messages:
+            st.chat_message(msg.role).write(msg.content)
 
     def execute(*args, **kwargs):
         func(*args, **kwargs)
@@ -46,7 +45,7 @@ def display_msg(msg, author):
         msg (str): message to display
         author (str): author of the message -user/assistant
     """
-    st.session_state.messages.append({"role": author, "content": msg})
+    st.session_state.messages.append(ChatMessage(role=author, content=msg))
     st.chat_message(author).write(msg)
 
 def configure_openai_api_key():
@@ -59,14 +58,39 @@ def configure_openai_api_key():
         )
     if openai_api_key.startswith('sk-'):
         st.session_state['OPENAI_API_KEY'] = openai_api_key
-        os.environ['OPENAI_API_KEY'] = openai_api_key
+        if not 'OPENAI_API_KEY' in os.environ or os.environ['OPENAI_API_KEY'] != openai_api_key:
+            os.environ['OPENAI_API_KEY'] = openai_api_key
+            st.rerun()
     else:
         st.warning('Please enter a valid OpenAI API key!', icon='⚠️')
         st.info("Obtain your key from this link: https://platform.openai.com/account/api-keys")
         st.stop()
     return openai_api_key
 
+
+def configure_model():
+    if 'OPENAI_API_KEY' in os.environ:
+        openai_model = st.sidebar.selectbox(
+            "Select Model:",
+            models,
+            index = 0
+        )
+        st.session_state['OPENAI_MODEL'] = openai_model
+
 def reset_chat():
     if "messages" in st.session_state:
         del st.session_state["messages"] 
     
+def configure_new_chat():
+    st.sidebar.markdown("<br><br>", unsafe_allow_html=True)
+    st.sidebar.button("New Chat", on_click=reset_chat)    
+
+def check_key():
+    if "current_key" not in st.session_state or st.session_state["current_key"] != st.session_state['OPENAI_API_KEY']:
+        del st.session_state["messages"]
+        st.rerun()
+
+def check_model():
+    if "current_model" not in st.session_state or st.session_state["current_model"] != st.session_state['OPENAI_MODEL']:  
+        del st.session_state["messages"]
+        st.rerun()
